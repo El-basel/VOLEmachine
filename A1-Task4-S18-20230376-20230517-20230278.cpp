2d8942cb7b88c3fd0d3c8934da4237e93ce979ce
y0ussefwalid <youssefwalid0505@gmail.com>
2024-11-07 17:48:34 +0200

    Revert "handled an error in the add to screen"
    This reverts commit 72a063213d64f37858e9d81858db0d57426fc525.

--- A1-Task4-S18-20230376-20230517-20230278.cpp
+++ A1-Task4-S18-20230376-20230517-20230278.cpp
@@ -63,10 +63,14 @@ void Memory::reset() {
 }
 
 void Memory::addtoScreen(std::string value) {
-    int size = screenbuffer.str().size();
-    if (size % 16 == 0) screenbuffer << '\n';
-    if (value.size() == 1) value = "0" + value;
-    screenbuffer << setw(3) << value;
+    static int counter = 0;
+    static int lines = 1;
+    if ((counter + value.size()) > lines*10) {
+        ++lines;
+        screenbuffer << '\n';
+    }
+    screenbuffer << ' ' << value;
+    counter += value.size();
 }
 
 std::string Memory::getScreen() {
@@ -384,7 +388,7 @@ void Machine::execute(bool& asWhole) {
 }
 void Machine::displayscreen(){
     std::cout << "\n------- Screen -------\n";
-    std::cout << memory.getScreen();
+    std::cout << std::setw(10) << std::left <<memory.getScreen();
     std::cout << "\n----------------------\n";
 }
 void Machine::outputState() {
